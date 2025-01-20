d5950ce20b3a6a9396e2b6e9fbb203ba987ad67c
wilsonsanz <wilsonsanz17@hotmail.com>
2025-01-20 00:09:21 -0500

    Initial commit

---
+++ Ejercicio1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // Importa el espacio de nombres "System", que contiene funcionalidades básicas como entrada/salida.
+
+class Program // Define una clase llamada "Program", que es el punto de entrada del programa.
+{
+    static void Main(string[] args) // Método principal, donde comienza la ejecución del programa.
+    {
+        // Inicia un bucle "for" que recorre desde 0 hasta 100, incrementando de 2 en 2.
+        for (int i = 0; i <= 100; i += 2)
+        {
+            // Imprime el valor actual de la variable "i" en la consola.
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Ejercicio1_1/README.md
@@ -0,0 +1,9 @@
+# Proyecto: Números Pares de 0 a 100 en C#
+
+Este proyecto en C# imprime todos los números pares entre 0 y 100 utilizando un bucle `for`.
+
+## Funcionalidades
+
+- El programa utiliza un bucle `for` para iterar desde 0 hasta 100, incrementando de 2 en 2.
+- En cada iteración, se imprime el valor de la variable `i`, que representa el número par actual.
+
